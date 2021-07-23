d1754f5f4c35fe89eda9fe714f4940248d1bcba3
WBoxue <86951515+WBoxue@users.noreply.github.com>
2021-07-23 16:10:39 +0800

    Delete serializer.py

--- serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from app1.models import History
-
-
-class HistorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = History
-        fields = '__all__'
